1a5216527a0d1c3c25e10a94923a4c973a1edc18
Guilherme Schá <guilhermescha@live.com>
2020-08-29 19:35:14 -0300

    Renamed Genders files to Genres files

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UserSeeder::class);
         $this->call(CategoriesTableSeeder::class);
-        $this->call(GendersTableSeeder::class);
+        $this->call(GenresTableSeeder::class);
     }
 }
